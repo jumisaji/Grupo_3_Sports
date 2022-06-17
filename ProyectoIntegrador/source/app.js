440f43a8b1495787f1663a6631525b9e7cf8d79e
jumisaji <ing.jsanchezj@gmail.com>
2022-06-17 04:31:33 -0500

    Julie-Termino las estructuras de html de MBT y Urban y agrego estructura para logo en el footer

--- ProyectoIntegrador/source/app.js
+++ ProyectoIntegrador/source/app.js
@@ -13,5 +13,7 @@ server.use(express.static(public));
 server.get('/', (req,res) => res.sendFile(path.resolve(__dirname, '../views/home.html')))
 server.get('/ruta', (req,res) => res.sendFile(path.resolve(__dirname, '../views/ruta.html')))
 server.get('/bmx', (req,res) => res.sendFile(path.resolve(__dirname, '../views/bmx.html')))
+server.get('/mtb', (req,res) => res.sendFile(path.resolve(__dirname, '../views/mtb.html')))
+server.get('/urban', (req,res) => res.sendFile(path.resolve(__dirname, '../views/urban.html')))
 
 server.get('/', (req,res) => res.send('Bienvenid@ a Go Bike'));
