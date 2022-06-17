cf2ef0d0ddf10493d67a372c27a373336013d182
jumisaji <ing.jsanchezj@gmail.com>
2022-06-17 01:30:25 -0500

    Julie-Se crean archivo app.js, se inicializa express y se establece en package.jason test y start

---
+++ ProyectoIntegrador/source/app.js
@@ -0,0 +1,15 @@
+
+const path = require('path');
+const express = require('express');
+const server = express();
+
+const port = process.env.PORT || 2022;
+server.listen(port, () => console.log('Abriendo el servidor http://localhost:'+port));
+
+const public = path.resolve(__dirname, '../public');
+
+server.use(express.static(public));
+
+server.get('/', (req,res) => res.sendFile(path.resolve(__dirname, '../views/home.html')))
+
+server.get('/', (req,res) => res.send('Bienvenid@ a Go Bike'));
