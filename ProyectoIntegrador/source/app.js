1f42cb7b770df6499d521ba3fd3766081fd83850
waltervhite <juanchiperez2004@gmail.com>
2022-06-18 21:54:30 -0300

    login juan cruz

--- ProyectoIntegrador/source/app.js
+++ ProyectoIntegrador/source/app.js
@@ -15,5 +15,6 @@ server.get('/ruta', (req,res) => res.sendFile(path.resolve(__dirname, '../views/
 server.get('/bmx', (req,res) => res.sendFile(path.resolve(__dirname, '../views/bmx.html')))
 server.get('/mtb', (req,res) => res.sendFile(path.resolve(__dirname, '../views/mtb.html')))
 server.get('/urban', (req,res) => res.sendFile(path.resolve(__dirname, '../views/urban.html')))
+server.get('/login', (req,res) => res.sendFile(path.resolve(__dirname, '../views/login.html')))
 
 server.get('/', (req,res) => res.send('Bienvenid@ a Go Bike'));
