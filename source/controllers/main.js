d80b2482d5f2650aa9fcbc65a6e732aafc11b577
jumisaji <ing.jsanchezj@gmail.com>
2022-06-30 19:55:12 -0500

    Julie ajusto Package.json

--- source/controllers/main.js
+++ source/controllers/main.js
@@ -1,19 +1,9 @@
 const {resolve} = require('path')
 module.exports = {
     
-home: function(req,res){ let file = resolve(__dirname, '../views/home.html') 
-res.sendFile(file)}
-
-
-
-
-
-
-
-
-
-
-
-
+home: function(req,res){ 
+    let file = resolve(__dirname, '../views/home.html') 
+    res.sendFile(file)
+}
 }
 
