384c763d9f449ee7d4399f4835184ddf9c15c65c
jumisaji <ing.jsanchezj@gmail.com>
2022-07-14 08:59:38 -0500

    Julie- Cambio nombreas a inglés de controllers users y main

--- source/controllers/main.js
+++ source/controllers/main.js
@@ -5,7 +5,7 @@ const data = [
 ];
 const controller = {
     home: (req,res) => res.render('home', {
-     styles: ['styles_main_home', 'animaciones_main_home', 'media_queries_main_home']   
+     styles: ['styles_main_home', 'animations_main_home', 'media_queries_main_home']   
     }),
 };
 module.exports = controller;
--- source/controllers/users.js
+++ source/controllers/users.js
@@ -5,15 +5,15 @@ const data = [
 ];
 const controller = {
     login: (req,res) => res.render('login', {
-        styles: ['styles_login', 'animaciones_login', 'media_queries_formularios']
+        styles: ['styles_login', 'animations_login', 'media_queries_forms']
     }),
 
     register: (req,res) => res.render('register', {
-        styles: ['styles_register', 'animaciones_register', 'media_queries_formularios']
+        styles: ['styles_register', 'animations_register', 'media_queries_forms']
     }),
 
-    carrito: (req,res) => res.render('carrito', {
-        styles: ['styles_carrito', 'animaciones_carrito', 'media_queries_carrito']
+    carrito: (req,res) => res.render('cart', {
+        styles: ['styles_cart', 'animations_cart', 'media_queries_cart']
     }),
 };
 module.exports = controller;
