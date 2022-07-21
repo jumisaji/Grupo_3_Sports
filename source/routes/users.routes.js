133aaeeba2de94ea58ca2c892ae4eff544d3bdac
MateoDV34 <matedellavedova@gmail.com>
2022-07-21 14:58:34 -0300

    Mate-Juan-Julie- Creacion de carpeta uploads, creacion de models

--- source/routes/users.routes.js
+++ source/routes/users.routes.js
@@ -2,7 +2,7 @@ const {Router} = require('express');
 const rutas = Router();
 const multer = require('multer');
 const storage = require('../modules/storage')
-const upload = multer({storage: storage('products')});
+const upload = multer({storage: storage('users')});
 
 const {login, register, carrito,creacion,edicion} = require('../controllers/users'); 
 rutas.get('/login',login);
