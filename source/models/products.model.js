e8adaeb020f803d47f425c5e8447866a6d695574
MateoDV34 <matedellavedova@gmail.com>
2022-07-21 15:35:39 -0300

    Mate-Juan-Julie- primera parte del crud y actualizacion del bikes.json

---
+++ source/models/products.model.js
@@ -0,0 +1,38 @@
+const { readFileSync, writeFileSync} = require('fs');
+const {resolve} = require('path');
+
+const model = {
+    app: function(){
+        let file = resolve(__dirname,'../data','bikes.json');
+        let data = readFileSync(file);
+        return JSON.parse(data);
+      },
+    one:function(id){
+        let file = resolve(__dirname,'../data','bikes.json');
+        let data = readFileSync(file);
+        let products = JSON.parse(data);
+        return products.find(product => product.id === id)
+      },
+    create: function(data){
+        let file = resolve(__dirname,'../data','bikes.json');
+        let info = readFileSync(file);
+        let products = JSON.parse(info);
+        let lastProduct= products[products.length - 1];
+        return Object({
+          id: products.length == 0 ? 1 : lastProduct.id + 1,
+          name: data.name,
+          price: parseInt(data.price),
+          description: data.description,
+          image: data.image,
+          category: data.category,
+          color: data.color
+        })
+      },
+    write: function(data) {
+        let file = resolve(__dirname,'../data','products.json');
+        let info = JSON.stringify(data,null,2);
+        return writeFileSync(file, info);
+      },
+    }
+    
+    module.exports = model;
