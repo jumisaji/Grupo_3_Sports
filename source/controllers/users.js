445f0a406ac8e34386d367f90af15c817ec329d0
MateoDV34 <matedellavedova@gmail.com>
2022-07-26 12:51:19 -0300

    login, register, cart

--- source/controllers/users.js
+++ source/controllers/users.js
@@ -1,16 +1,23 @@
 const {app,one,create,write} = require('../models/products.model');
 module.exports = {
-  login: (req,res) => res.render('login', {
+  login: (req,res) => {
+    return res.render('users/login', {
       styles: ['styles_login', 'animations_login', 'media_queries_forms']
-  }),
+  })
+  },
 
-  register: (req,res) => res.render('register', {
+  register: (req,res) => {
+    return res.render('users/register', {
       styles: ['styles_register', 'animations_register', 'media_queries_forms']
-  }),
+  })
+  },
 
-  carrito: (req,res) => res.render('cart', {
+  carrito: (req,res) => {
+    return res.render('users/cart', {
       styles: ['styles_cart', 'animations_cart', 'media_queries_cart']
-  }),
+  })
+  },
+  
   edicion: (req,res) => res.render('edit', {
       styles: [ 'styles_edit' ]
   }),
@@ -37,5 +44,30 @@ module.exports = {
         title: 'Edit of products',
         product:product 
       })
-    }
+    },
+  modify: (req, res) => {
+      let product = one(parseInt(req.params.id))
+      let products = index();
+      let productsModified = products.map(p =>{ 
+        if(p.id == product.id){
+          p.name =  req.body.name
+          p.description = req.body.description
+          p.price = parseInt(req.body.price)
+          p.image = req.files && req.files.length > 0 ? req.files[0].filename : p.image
+        }
+        return p 
+      });
+      write(productsModified)
+      return res.redirect('/products/detail/' + product.id)
+    },
+  destroid:(req,res) => {
+      let product = one(parseInt(req.params.id))
+      if(!product){
+        return res.redirect('/products/');
+      }
+      let products = index();
+      let productsDeleted = products.filter(p => p.id !== product.id)
+      write(productsDeleted)
+      return res.redirect('/products/');
+    }  
 };
--- source/routes/users.routes.js
+++ source/routes/users.routes.js
@@ -4,13 +4,16 @@ const multer = require('multer');
 const storage = require('../modules/storage')
 const upload = multer({storage: storage('users')});
 
-const {login, register, carrito, create, edicion, save } = require('../controllers/users'); 
+const {login, register, carrito, create, edit, save, modify, destroid } = require('../controllers/users'); 
 
 rutas.get('/login',login);
 rutas.get('/register',register);
 rutas.get('/cart',carrito);
 rutas.get('/create', create)
 rutas.post('/save',[upload.any()], save)
+rutas.get('/edit/:id', edit)
+rutas.put('/edit/:id',[upload.any()], modify)
+rutas.delete('/delete/:id',destroid)
 
 
 module.exports = rutas
