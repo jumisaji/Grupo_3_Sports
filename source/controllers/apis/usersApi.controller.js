dfb3c30cb0d63b0ae57fd126d0c73974d7f6b14f
MateoDV34 <matedellavedova@gmail.com>
2022-10-01 23:10:57 -0300

    implementación de busqueda por id en API de usuarios

--- source/controllers/apis/usersApi.controller.js
+++ source/controllers/apis/usersApi.controller.js
@@ -16,4 +16,41 @@ module.exports = {
             return res.status(500).json(error);
         }
     },
+    one: async (req, res) =>{
+        try {
+            let userDB = await User.findByPk(
+                req.params.id, {
+                    include: {
+                        all: true
+                    }
+                }
+            )
+            if(userDB){
+
+                return res.status(200).json(userDB);
+            }else{
+                return res.status(404).json('No se encontró este usuario.');
+            }
+        } catch (error) {
+            return res.status(500).json(error);
+        }
+
+    },
+    /*process: async (req, res) =>{
+
+        try {
+            req.body.password = hashSync(req.body.password,10)
+            req.body.isAdmin = req.body.username.includes('@dh.com') ? true: false
+
+            let newUser = await User.create(req.body);
+
+            if(newUser){
+                return res.status(200).json(newUser);
+            }else{
+                return res.status(404).json('No se creó el usuario');
+            }
+        } catch (error) {
+            return res.status(500).json(error);
+        }
+    },*/
 }
--- source/routes/apis/usersApi.routes.js
+++ source/routes/apis/usersApi.routes.js
@@ -1,7 +1,11 @@
 const {Router} = require('express');
 const router = Router();
-const {all} = require('../../controllers/apis/usersApi.controller');
+const {all, one, process} = require('../../controllers/apis/usersApi.controller');
 
 router.get('/', all);
+
+router.get('/:id', one);
+
+//router.post('/', process);
 
 module.exports = router
