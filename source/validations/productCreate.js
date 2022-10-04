220c705f14cc70ecab3b657ebaca2a0ed2ea22ad
waltervhite <juanchiperez2004@gmail.com>
2022-10-04 11:54:55 -0300

    :)

--- source/validations/productCreate.js
+++ source/validations/productCreate.js
@@ -3,15 +3,17 @@ const {extname,resolve} = require('path')
 const {unlinkSync} = require('fs')
 
 module.exports = [
-    body('name').notEmpty().withMessage('Debe completar este campo').bail().
-    isLength({ min : 5 }).withMessage('El nombre tiene que completarse con un minimo de 5 caracteres').bail(),
+    body('name').notEmpty().withMessage('el campo del nombre debe estar completo').bail().
+    isLength({ min : 5 }).withMessage('El nombre del producto tiene que tener minimo de 5 caracteres').bail(),
 
-    body('price').notEmpty().withMessage('Debe completar este campo').bail(),
+    body('price').notEmpty().withMessage('el campo del precio debe estar completo').bail(),
+
     
+
     body('image').custom((value,{req}) =>{
              let archivos = req.files
              if(!archivos || archivos.length == 0){
-                 throw new Error('No se subio ninguna imagen')
+                 throw new Error('Debe subir una imagen')
              }
              let extensiones = ['.svg','.png','.jpg','.jpeg']
              let image = archivos[0]
@@ -19,7 +21,7 @@ module.exports = [
     
              if(!extensiones.includes(extension)){
                  unlinkSync(resolve(__dirname, '../../uploads','products',image.filename))
-                 throw new Error('La imagen no tiene una extension valida')
+                 throw new Error('La imagen no tiene una extension valida ')
              }
     
              if(image.size > 2097152){
@@ -29,7 +31,7 @@ module.exports = [
     
              return true
          }),
-    body('description').notEmpty().withMessage('Debe completar este campo').bail().
-    isLength({ min : 20 }).withMessage('La descripción tiene que completarse con un minimo de 20 caracteres').bail(),
+    body('description').notEmpty().withMessage('el campo de la descripcion debe estar completo').bail().
+    isLength({ min : 20 }).withMessage('La descripción del producto tiene que tener mínimo de 20 caracteres').bail(),
 
 ]
