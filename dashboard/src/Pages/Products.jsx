7876c97d029f42d2416a9228d96b18137669aa04
jumisaji <ing.jsanchezj@gmail.com>
2022-10-11 16:41:50 -0500

    Julie- Pagina productos y usuarios dasboard

--- dashboard/src/Pages/Products.jsx
+++ dashboard/src/Pages/Products.jsx
@@ -2,7 +2,15 @@ import React from 'react'
 
 const Products = () => {
   return (
-    <div>Products</div>
+  <div>
+    <h1>PRODUCTOSS GO BIKE</h1>
+    <div className='div-cont-products'>
+                  <p className='top-p-user'>Productos Registrados:</p>
+                  <ul>
+                      
+                  </ul>
+              </div>
+  </div>
   )
 }
 
--- dashboard/src/Pages/Users.jsx
+++ dashboard/src/Pages/Users.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 
 const Users = () => {
+  
   return (
     <div>
-
-      <h1> hi users</h1>
+      <h1>USUARIOS GO BIKE</h1>
+      <div className='div-cont-userS'>
+                    <p className='top-p-user'>Usuarios Registrados:</p>
+                    <ul>
+                        
+                    </ul>
+                </div>
       
-
 
     </div>
   )
